a95e7ffcca6efc94b95b98422a4ced1957dd468a
Carl Myers <cmyers@pernixdata.com>
2016-04-05 17:37:45 -0700

    Solved q2

--- q2/src/org/cmyers/euler/q2/Main.java
+++ q2/src/org/cmyers/euler/q2/Main.java
@@ -2,6 +2,23 @@ package org.cmyers.euler.q2;
 
 class Main {
     public static final void main(String [] args) {
+        // for fib numbers where n1 = 1, n2 = 2, ... find the sum of all even
+        // valued terms under size 4 mil
+        long maxTerm = 4000000;
+
+        long a = 1;
+        long b = 2;
+        long sum = 2;
+        while(b < maxTerm) {
+            long c = a + b;
+            a = b;
+            b = c;
+            if(c % 2 == 0) {
+                sum = sum + c;
+                System.out.println("Including term " + c + " so sum so far is " + sum);
+            }
+        }
+        System.out.println("Sum of even fib numbers (starting at 1,2) under 4 mil is " + sum);
     }
 }
 
